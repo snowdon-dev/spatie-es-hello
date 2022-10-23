90624dba073d39c6f4ee84fc26fb56864fbea805
Dale Snowdon <dale@snowdon.dev>
2022-10-23 18:55:06 +0100

    Pass data to form. Readme.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use InvalidArgumentException;
+use App\Models\Profile;
 
 class DashboardController extends Controller
 {
     public function show()
     {
-        return view('dashboard');
+        $profile = Profile::where('userId', auth()->user()->id)->first();
+
+        if (empty($profile)) {
+            throw new InvalidArgumentException("No profile projection - possible invalid request");
+        }
+        return view('dashboard', $profile->only('username'));
+    }
+
+
+    public function store() {
+        // update the event stream for user, not the active record
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
                             @csrf
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Title</label>
-                                <input type="text" id="username" name="username" class="form-control" required="">
+                                <input type="text" id="username" name="username" class="form-control" value="{{$username}}" required="">
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
